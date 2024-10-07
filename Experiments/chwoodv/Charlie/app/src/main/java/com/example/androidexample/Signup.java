b2e10b5740ed733d960036a2976eaf4ad9215bf1
chwoodv <148597237+chwoodv@users.noreply.github.com>
2024-10-07 14:57:19 -0500

    moved experiment

--- Experiments/chwoodv/Charlie/app/src/main/java/com/example/androidexample/Signup.java
+++ Experiments/chwoodv/Charlie/app/src/main/java/com/example/androidexample/Signup.java
@@ -14,7 +14,6 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
