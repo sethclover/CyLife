37dc82b8cafc94322ce3764d76df285f37f3a17a
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2024-10-11 14:26:20 -0500

    Added more comments

--- Backend/src/src/main/java/onetomany/Organisation/OrganisationRepository.java
+++ Backend/src/src/main/java/onetomany/Organisation/OrganisationRepository.java
@@ -2,5 +2,6 @@ package onetomany.Organisation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Organisation Repository
 public interface OrganisationRepository extends JpaRepository<Organisation, String> {
 }
