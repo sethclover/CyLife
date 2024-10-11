bdd5d71c94d424174cadc2b4b661ef712cc95cf8
Seth Clover <sclover@iastate.edu>
2024-10-11 17:32:23 -0500

    fixed merge in event

--- Backend/src/src/main/java/onetomany/Events/Event.java
+++ Backend/src/src/main/java/onetomany/Events/Event.java
@@ -14,33 +14,21 @@ public class Event {
     private int eventId;
     private String eventName;
     private String description;
-<<<<<<< HEAD
-    private String location_time;
-=======
     private String eventLocation;
->>>>>>> 7aa477fd833dcede8e9860ffcbff5c5c6b568338
 
     // Constructors
     public Event() {
     }
 
-    public Event(String eventName, String description, String location_time) {
+    public Event(String eventName, String description, String eventLocation) {
         this.eventName = eventName;
         this.description = description;
-        this.location_time = location_time;
+        this.eventLocation = eventLocation;
     }
 
     // Getters and Setters
     public int getEventId() {
         return eventId;
-    }
-
-    public String getEventLocationTime() {
-        return location_time;
-    }
-
-    public void setEventLocationTime(String location_time) {
-        this.location_time = location_time;
     }
 
     public void setEventId(int eventId) {
