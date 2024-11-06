7f4d2d21d81c7721c62233fc15b22fe71ba8bc1c
chwoodv <148597237+chwoodv@users.noreply.github.com>
2024-11-06 13:57:15 -0600

    fixed main again because git is dumb

--- Frontend/CyLife/app/src/main/java/com/example/cylife/WelcomeActivityClub.java
+++ Frontend/CyLife/app/src/main/java/com/example/cylife/WelcomeActivityClub.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivityClub extends AppCompatActivity {
 
     private TextView welcomeText;
+    private Button entButton;
     private Button logoutButton;
+    private Button clubButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,21 @@ public class WelcomeActivityClub extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_club);
 
         // Initialize views
-        welcomeText = findViewById(R.id.welcomeTextView);
+        welcomeText = findViewById(R.id.welcome_text);
+        entButton = findViewById(R.id.ent_button);
+        clubButton = findViewById(R.id.edit_club_button);
+        logoutButton = findViewById(R.id.logout_button);
 
+        entButton.setOnClickListener(view -> {
+            // Start the Events Activity
+            Intent intent = new Intent(WelcomeActivityClub.this, EventsActivity.class);
+            startActivity(intent);
+        });
+        clubButton.setOnClickListener(view -> {
+            // Start the Club Activity
+            Intent intent = new Intent(WelcomeActivityClub.this, EditClub.class);
+            startActivity(intent);
+        });
 
         logoutButton.setOnClickListener(view -> {
             finish();
