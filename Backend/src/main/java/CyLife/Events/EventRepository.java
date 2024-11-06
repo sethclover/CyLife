fe327399f0eafd272db9202d00be70530f7b606a
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2024-11-06 14:52:29 -0600

    Blah blah blah

--- Backend/src/main/java/CyLife/Events/EventRepository.java
+++
@@ -1,7 +0,0 @@
-
-package CyLife.Events;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
--- Backend/src/main/java/CyLife/Websockets/chat/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package CyLife.Websockets.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long>{
-
-}
