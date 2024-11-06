66768c5414323361fa73f48425e83d9ac4c0e77e
chwoodv <148597237+chwoodv@users.noreply.github.com>
2024-11-06 10:10:10 -0600

    Please let me merge

--- Frontend/CyLife/app/src/main/java/com/example/cylife/WelcomeActivityClub.java
+++ Frontend/CyLife/app/src/main/java/com/example/cylife/WelcomeActivityClub.java
@@ -1,4 +1,28 @@
 package com.example.cylife;
 
-public class WelcomeActivityClub {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivityClub extends AppCompatActivity {
+
+    private TextView welcomeText;
+    private Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_club);
+
+        // Initialize views
+        welcomeText = findViewById(R.id.welcomeTextView);
+
+
+        logoutButton.setOnClickListener(view -> {
+            finish();
+        });
+    }
 }
