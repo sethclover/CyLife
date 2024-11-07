50b2e3212f36da16d955b702b6c291d89259af7c
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2024-11-07 17:12:40 -0600

    Second main feature is done

---
+++ Backend/CyLife/src/main/java/CyLife/Clubs/ClubRequestRepository.java
@@ -0,0 +1,7 @@
+package CyLife.Clubs;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// ClubRequest Repository
+public interface ClubRequestRepository extends JpaRepository<ClubRequest, Integer> {
+}
