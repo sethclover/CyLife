b53a756c4c8396325c20e18ad12adcd214fc4f8b
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2024-11-08 16:08:23 -0600

    Done

--- Backend/CyLife/src/main/java/CyLife/Clubs/Clubs/ClubRepository.java
+++
@@ -1,7 +0,0 @@
-package CyLife.Clubs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Club Repository
-public interface ClubRepository extends JpaRepository<Club, Integer> {
-}
--- Backend/CyLife/src/main/java/CyLife/Clubs/Clubs/ClubRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package CyLife.Clubs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// ClubRequest Repository
-public interface ClubRequestRepository extends JpaRepository<ClubRequest, Integer> {
-}
