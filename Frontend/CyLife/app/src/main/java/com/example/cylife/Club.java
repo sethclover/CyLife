c33243a40548d07af831f28e08bfe469fd5938ee
Dhvani Mistry <dhvanimistry7@gmail.com>
2024-11-27 15:57:18 -0600

    Fetching id from response

--- Frontend/CyLife/app/src/main/java/com/example/cylife/Club.java
+++ Frontend/CyLife/app/src/main/java/com/example/cylife/Club.java
@@ -1,13 +1,19 @@
 package com.example.cylife;
 
 public class Club {
-    private final String name;
+  private final String name;
+  private String id;
 
-    public Club (String name){
-        this.name = name;
-    }
+  public Club(String name, String id) {
+    this.id = id;
+    this.name = name;
+  }
 
-    public String getName(){
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public String getId() {
+    return id;
+  }
 }
